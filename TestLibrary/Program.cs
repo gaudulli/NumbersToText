378d4123b83c7bb48e81c49f11c968100876d56f
ricgaudet <ricgaudet@nmlegis.gov>
2016-01-20 14:20:34 -0700

    Jan 20 commit

---
+++ TestLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumbersToText;
+
+namespace TestLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = NumToText.ConvertToText((decimal).125, false, 3, false);
+            //string text = NumToText.ConvertToCurrency((decimal).353, false, 3);
+            string text = NumToText.ConvertToLegalDollarText((decimal)45.22,2,false);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
